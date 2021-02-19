9b69ffafd331b0b45dd7e31e6f7847cc46b8ca27
Chunxian Zhang <1836891291@qq.com>
2021-02-19 17:45:19 +0800

    update CSS 浮动

---
+++ JavaScript/demo.js
@@ -0,0 +1,11 @@
+/* function functionName(number) {
+    return number * number;
+}
+
+console.log(functionName(10)); */
+
+function sumAllNums() {
+    console.log(arguments)
+}
+
+sumAllNums(1, 2, 3, 4);
